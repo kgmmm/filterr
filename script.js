7a0a8365b0ad4621ef1d54a9c31e819e89a65cbf
kgmmm <kgmofficial0@gmail.com>
2016-01-03 21:22:16 +0000

    Image into canvas
    Input type = file to add image to canvas, resizes larger(than canvas)
    images, places them in the center canvas and maintains a 20px border
    around the image. Added skinny loader when image is being added to
    canvas.

--- script.js
+++ script.js
@@ -1,3 +1,99 @@
 $(function() {
 	$('.svgload').load('../svg/svgdefs.html');
+
+	$('.imgcanv')[0].width = window.innerWidth;
+	$('.imgcanv')[0].height = window.innerHeight - 150;
+
+	$('.submitload').on('click', loadImg);
 });
+
+function loadImg() {
+	var input = $('.loadimg')[0],
+		file,
+		fr,
+		img;
+
+
+	$('.loader').toggleClass('hide');
+
+	if(typeof window.FileReader !== 'function') {
+		alert('FILTERR is not supported by your browser!');
+		return false;
+	}
+
+	if(!input) {
+		alert('No file!');
+	} else if(!input.files) {
+		alert('Browser does not support "files" property of file inputs.');
+	} else if(!input.files[0]) {
+		alert('Please select a file first!');
+	} else {
+		file = input.files[0];
+		fr = new FileReader();
+		fr.onload = createImg;
+		fr.readAsDataURL(file);
+	}
+
+	function createImg() {
+		img = new Image();
+		img.onload = imgLoaded;
+		img.src = fr.result;
+	}
+
+	function imgLoaded() {
+		var canvas = $('.imgcanv')[0],
+			context = canvas.getContext('2d'),
+			imgWidth = img.naturalWidth,
+			imgHeight = img.naturalHeight,
+			canvWidth = canvas.width,
+			canvHeight = canvas.height;
+
+		if(imgWidth > imgHeight) {
+			var newHeight = imgHeight / canvHeight * 100;
+
+			if(newHeight <= 100) {
+				$('main').addClass('hasimg');
+				context.clearRect(0, 0, canvWidth, canvHeight);
+				context.drawImage(img,
+					canvWidth / 2 - (imgWidth - 40) / 2,
+					canvHeight / 2 - (imgHeight - 40) / 2,
+					imgWidth - 40, imgHeight - 40);
+				$('.loader').toggleClass('hide');
+			} else {
+				var percent = newHeight - 100,
+					drawHeight = imgHeight - imgHeight / 100 * percent;
+
+				$('main').addClass('hasimg');
+				context.clearRect(0, 0, canvWidth, canvHeight);
+				context.drawImage(img,
+					canvWidth / 2 - (canvWidth - 40) / 2,
+					canvHeight / 2 - (drawHeight - 40) / 2,
+					canvWidth - 40, drawHeight - 40);
+				$('.loader').toggleClass('hide');
+			}
+		} else if(imgHeight >= imgWidth) {
+			var newWidth = imgHeight / canvHeight * 100;
+
+			if(newWidth <= 100) {
+				$('main').addClass('hasimg');
+				context.clearRect(0, 0, canvWidth, canvHeight);
+				context.drawImage(img,
+					canvWidth / 2 - (imgWidth - 40) / 2,
+					canvHeight / 2 - (imgHeight - 40) / 2,
+					imgWidth - 40, imgHeight - 40);
+				$('.loader').toggleClass('hide');
+			} else {
+				var factor = newWidth / 100,
+					drawWidth = imgWidth / factor;
+
+				$('main').addClass('hasimg');
+				context.clearRect(0, 0, canvWidth, canvHeight);
+				context.drawImage(img,
+					canvWidth / 2 - (drawWidth - 40) / 2,
+					canvHeight / 2 - (canvHeight - 40) / 2,
+					drawWidth - 40, canvHeight - 40);
+				$('.loader').toggleClass('hide');
+			}
+		}
+	}
+}
